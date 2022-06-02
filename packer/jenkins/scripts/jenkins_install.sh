2950c14f036877210cd673cb1a97c77e9d303d98
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2022-06-02 15:14:37 +0900

    Update jenkins_install.sh

--- packer/jenkins/scripts/jenkins_install.sh
+++ packer/jenkins/scripts/jenkins_install.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 # install jenkins
+# repository 업데이트
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade
+
+#jenkins 동작을 위한 openjdk 설치
 sudo amazon-linux-extras install java-openjdk11 -y
+
+#jenkins 설치
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+#jenkins default password
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
-# install harbor
+# docker 설치
 sudo yum install -y docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
-# jenkins user permission to docker
+# jenkins user가 dockerfile을 빌드하기 위한 권한을 추가
 sudo usermod -aG docker jenkins
 sudo service jenkins restart
