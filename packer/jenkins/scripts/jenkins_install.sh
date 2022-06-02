fc8a083fa9d8e6d9e6e857e453304c97c73246be
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2022-06-02 15:17:08 +0900

    Update jenkins_install.sh

--- packer/jenkins/scripts/jenkins_install.sh
+++ packer/jenkins/scripts/jenkins_install.sh
@@ -7,10 +7,8 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade
 
-#jenkins 동작을 위한 openjdk 설치
-sudo amazon-linux-extras install java-openjdk11 -y
-
 #jenkins 설치
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
